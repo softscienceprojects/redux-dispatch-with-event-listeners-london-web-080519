ed28bbffae3951f8d828fe27b2ab1f926dbc875e
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-25 09:20:54 +0100

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,49 @@
-// add code snippets from README
+// action -> reducer -> new state
+
+// let state = {count: 0}
+
+// function reducer(state, action) {
+//     switch (action.type) {
+//         case 'INCREASE_COUNT':
+//             return {count: state.count + 1};
+//             default:
+//                 return state;
+//     }
+// }
+
+// function dispatch(action) {
+//     state = reducer(state, action);
+//     render()
+// }
+
+// function render() {
+//     let container = document.getElementById('container');
+//     container.textContent = state.count;
+// }
+
+let state;
+function reducer(state = {count: 0}, action) {
+    switch (action.type) {
+        case 'INCREASE_COUNT':
+            return {count: state.count + 1};
+            default:
+                return state;
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action);
+    render()
+}
+
+function render() {
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+}
+
+dispatch({type: '@@init'})
+
+let button = document.getElementById('button');
+button.addEventListener('click', () => {
+    dispatch({type: 'INCREASE_COUNT'})
+})
